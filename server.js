1681c1e6e0f83533199504c9676b04c2fdc56f7d
unknown <Ceyh4n>
2021-03-22 22:43:18 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./app/models");
+const dbConfig = require('./app/config/db.config');
+const Role = db.role;
+
+const app = express();
+
+db.mongoose
+  .connect(`${dbConfig.DB}`)
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+var corsOptions = {
+  origin: "http://localhost:3001"
+};
+
+app.use(cors(corsOptions));
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to user authentication app." });
+});
+
+// routes
+require('./app/routes/user.routes')(app);
+require('./app/routes/auth.routes')(app);
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
